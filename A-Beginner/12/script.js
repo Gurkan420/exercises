80fa786113714f4c776e6ffb3d6df9aebc1b20e6
Gurkan420 <august.ljungdell@skola.botkyrka.se>
2020-10-14 14:38:08 +0200

    log out 12

--- A-Beginner/12/script.js
+++ A-Beginner/12/script.js
@@ -1,5 +1,7 @@
-const x = myFunction(3, 3);  
-
 function myFunction(a, b) {
   return a * b;             
 }
+
+console.log(myFunction(3, 9))
+console.log(myFunction(4, 4))
+console.log(myFunction(3, 1))
